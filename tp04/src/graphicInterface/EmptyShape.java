b443c4c4814e3934f938e1fb82ff3af2a0488a8c
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-02-16 18:02:29 +0100

    Ajout de la classe EmptyShape

---
+++ tp04/src/graphicInterface/EmptyShape.java
@@ -0,0 +1,16 @@
+package graphicInterface;
+
+import java.awt.Color;
+
+public class EmptyShape extends BoxShape{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmptyShape(int x, int y) {
+		super(x, y, "empty", Color.pink);
+	}
+
+}

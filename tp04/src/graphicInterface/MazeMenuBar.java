233be43f9f8d335f9de73c66f7cf9eae9e2a8506
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-02-19 17:10:25 +0100

    Affichage du dialogue pour expliquer comment l'interface marche

--- tp04/src/graphicInterface/MazeMenuBar.java
+++ tp04/src/graphicInterface/MazeMenuBar.java
@@ -16,23 +16,35 @@ public class MazeMenuBar extends JMenuBar{
 		//add ces menu au menuBar
 		this.add(fileMenu = new JMenu("File"));
 		this.add(optionMenu = new JMenu("Options"));
-		this.add(helpMenu = new JMenu("Help"));
+		this.add(helpMenu = new JMenu("?"));
+		
 		//Mettre les items de chaque menu.
 		JMenuItem  resetItem= new JMenuItem("Reset");
         JMenuItem  quitItem= new JMenuItem("Quit");
         JMenuItem saveItem = new JMenuItem("Save the maze solution");
         JMenuItem importItem = new JMenuItem("Import maze file");
-        
+        JMenuItem help = new JMenuItem("help");
         fileMenu.add(importItem);
         fileMenu.add(saveItem);
         optionMenu.add(resetItem);
         optionMenu.add(quitItem);
-        
+        helpMenu.add(help);
         //Ajouter l'action de quitItem
         quitItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 System.exit(0);
             }
+        });
+        
+        help.addActionListener(e -> {
+            JOptionPane.showMessageDialog(mainApp,
+                    "You can give the maze you want to solve in two different ways : \n    -import it as a text file using these keys 'E', 'D', 'A', 'W'.\n"
+                    + "    -draw it manually in the display panel, you should select the type of the box in the configuration panel and click on the boxes you want to be the type selected.\n"
+                    + "Once you finish representing your maze, click on showShortestPath to show the solution\n"
+                    + "Enjoy !!\n"
+                    + "Rq: Telecom Paris is the best school ever ",
+                    "How to use the interface",
+                    JOptionPane.INFORMATION_MESSAGE);
         });
 	}
 

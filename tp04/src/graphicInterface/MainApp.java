6221d42b5f4a35ba6e47069563fb85c04d138468
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-03-01 12:12:08 +0100

    mise en commentaires de la classe MainApp

--- tp04/src/graphicInterface/MainApp.java
+++ tp04/src/graphicInterface/MainApp.java
@@ -2,39 +2,52 @@ package graphicInterface;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
+
+/**
+* The MainApp class represents the main application window for the program. It extends
+* the JFrame class to provide a graphical user interface and implements the ChangeListener
+* interface to handle changes to components within the frame.
+*/
 public class MainApp extends JFrame implements ChangeListener{
 	/**
 	 * 
 	 */
-	private MazeAppModel mazeAppModel; //references vers le model
+	private MazeAppModel mazeAppModel;
 	
-	private MainPanel mainPanel;
-	private MazeMenuBar menuBar;
+	private final MainPanel mainPanel;
+	private final MazeMenuBar menuBar;
 	private static final long serialVersionUID = 1L;
-	//La methode main a exécuter
+	
+	/**
+     * Launches the application.
+     * @param args command line arguments (not used)
+     */
 	public static void main(String args[]) {
         new MainApp();
     }
-	//Constructeur de l'application
+	
+	/**
+	 * Constructs a new MainApp object.
+	 */
     public MainApp() {
         super("Maze Solver");
         mazeAppModel=new MazeAppModel();
         mazeAppModel.addObserver(this);
         mainPanel= new MainPanel(this);
         menuBar=new MazeMenuBar(this);
-        //Ajouter ces attribut a l'affichage de la MainApp.
+        //Setting the mainPanel and the menuBar in the MainApp.
         setContentPane(mainPanel);
         setJMenuBar(menuBar);
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(1000,600));
         
-        //centrage de la fenetre 
+        //Centering the window.
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(dim.width/5 - getWidth()/5, dim.height/5 - getHeight()/5);
         
         
-        //Set an image for the application.
+        //Set the Logo I created as icon of the application.
         ImageIcon icon = new ImageIcon("logoGUI.png");
         setIconImage(icon.getImage());
         
@@ -49,9 +62,14 @@ public class MainApp extends JFrame implements ChangeListener{
 	public void setMazeAppModel(MazeAppModel mazeAppModel) {
 		this.mazeAppModel = mazeAppModel;
 	}
-	public MainPanel getMainPanel() {return mainPanel;} 
+	public MainPanel getMainPanel() {return mainPanel;}
+	
+	 /**
+     * Responds to changes in components within the frame.
+     * It is supposed to redraw the window in function of the model.
+     * @param e the ChangeEvent that occurred
+     */
 	@Override
-	//Cette méthode est censée redessiner la fenêtre en fonction du modèle MazeAppModel.
 	public void stateChanged(ChangeEvent e) {
 		mainPanel.notifyForUpdate();
 	}

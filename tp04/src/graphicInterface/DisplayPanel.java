a54200fa58b6368006d8cb98d5c15aecf5bea455
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-03-01 13:25:05 +0100

    mise en commentaires de la classe DisplayPanel

--- tp04/src/graphicInterface/DisplayPanel.java
+++ tp04/src/graphicInterface/DisplayPanel.java
@@ -4,19 +4,32 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/**
+ * The DisplayPanel class is a JPanel that displays our maze and listens for mouse events.
+ * It will be the panel that let the user interact with the maze.
+ */
 public class DisplayPanel extends JPanel implements MouseListener{
     private static final long serialVersionUID = 1L;
     private final MainApp mainApp;
     
-    //private BoxShape[][] map;
+    /**
+     * Constructs a new DisplayPanel object with a reference to the given MainApp object.
+     * It adds MouseListener to it and set the background in white.
+     * @param mainApp the MainApp object that this display panel is associated with.
+     */
 	public DisplayPanel(MainApp mainApp) {
         super();
         this.mainApp=mainApp;
         setBackground(Color.WHITE);
-        //map=mainApp.getMazeAppModel().getBoxes();
-        //Ajout du listener de la souris 
         addMouseListener(this);
     }
+	/**
+	 * Overrides the default paintComponent method to draw the BoxShape objects contained in the DisplayPanel's associated
+	 * MainApp object on the panel. The method retrieves the BoxShape objects from the MainApp's MazeAppModel, and then
+	 * uses the Graphics object to draw each box as a filled and outlined polygon with the appropriate color.
+	 *
+	 * @param g the Graphics object used to paint the panel and draw the BoxShape objects.
+	 */
 	 @Override
 	 public void paintComponent(Graphics g) {
 	   super.paintComponent(g);
@@ -32,21 +45,25 @@ public class DisplayPanel extends JPanel implements MouseListener{
 		 }
 	   
 	 }
-	 //getter of the map
-	 //public BoxShape[][] getMap(){
-		 //return map;
-	 //}
 	 
-	
+	/**
+	 * Invoked when a mouse button is pressed on the component, and if a box is pressed, it will change her type depending on the 
+	 * selected type in the model.
+	 * @param e the MouseEvent representing the button press
+	 */
 	public void mousePressed(MouseEvent e) {
 		 int w= mainApp.getMazeAppModel().getWidth();
 		 int h = mainApp.getMazeAppModel().getHeight();
-		 
+		 //We get the matrix of boxes from the model.
 		 BoxShape[][] map=mainApp.getMazeAppModel().getBoxes();
 		 for (int i=0;i<w;i++) {
 	    	for (int j=0;j<h;j++) {
 	    		BoxShape s=map[i][j];
 	    		if (s.contains(e.getPoint())) {
+	    			//if the box we want to change the type is departure or arrival, we should decrement the correspondent counter.
+	    			if (s.getLabel()=="D"){mainApp.getMazeAppModel().setnDepartures(mainApp.getMazeAppModel().getnDepartures() - 1);}
+	    			if (s.getLabel()=="A"){mainApp.getMazeAppModel().setnArrivals(mainApp.getMazeAppModel().getnArrivals() - 1);}
+	    			//We get the selected type from the model.
 	    			String sel = mainApp.getMazeAppModel().getSelectedType();
 	    			if (sel !=null) {
 	    				switch (sel) {
@@ -56,11 +73,13 @@ public class DisplayPanel extends JPanel implements MouseListener{
 			    				break;
 			    			case("d"):
 			    				map[i][j] = new DepartureShape(s.getX(), s.getY());
+			    				//incrementing the number of departures.
 			    				mainApp.getMazeAppModel().setnDepartures(mainApp.getMazeAppModel().getnDepartures() + 1);
 				    			repaint();
 			    				break;
 			    			case("a"):
 			    				map[i][j] = new ArrivalShape(s.getX(), s.getY());
+			    				//incrementing the number of arrivals
 			    				mainApp.getMazeAppModel().setnArrivals(mainApp.getMazeAppModel().getnArrivals() + 1);
 				    			repaint();
 			    				break;
@@ -75,11 +94,15 @@ public class DisplayPanel extends JPanel implements MouseListener{
 	    		
 	    	}
 	      
-		 }
-	 }
-	 public void notifyForUpdate() {
-		   repaint();
 		}
+	}
+	
+	/**
+	 * Notifies the displayPanel that it should update its state and be repainted.
+	*/
+	public void notifyForUpdate() {
+		repaint();
+	}
 	 
 	 
 

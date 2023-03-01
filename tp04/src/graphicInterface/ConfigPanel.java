a6b9abe2b9cb617b758de1fc7109645fedc64967
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-03-01 13:59:17 +0100

    mise en commentaires de la classe ConfigPanel

--- tp04/src/graphicInterface/ConfigPanel.java
+++ tp04/src/graphicInterface/ConfigPanel.java
@@ -2,28 +2,33 @@ package graphicInterface;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The ConfigPanel class is a JPanel that will help the user to configure the maze he want to solve, he can change the dimensions and selected type..
+ * It will be the panel that let the user interact with the maze.
+ */
 public class ConfigPanel extends JPanel{
-	private MainApp mainApp;
+	private final MainApp mainApp;
 	private final HeightField hField;
 	private final WidthField wField;
-	
-
-	private OkButton okButton; //Boutton de confirmation de choix 
-	private ChoiceBoxPanel cBoxPanel; // Le panel qui contient les differents types de boxes
-	
+	private final OkButton okButton; 
+	private final ChoiceBoxPanel cBoxPanel; 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+     * Constructs a new ConfigPanel object with a reference to the given MainApp object.
+     * It sets his position in the MainApp, his color, and it adds to it a new ChoicePanel,
+     * a new OkButton, and new field for height and width.
+     * It adds to this ConfigPanel an icon of TELECOM PARIS.
+     * @param mainApp the MainApp object that this display panel is associated with.
+     */
 	public ConfigPanel(MainApp mainApp) {
 		super();
 		this.mainApp=mainApp;
-		//normalement le size doit dependre du size de la fenetre.
 		setPreferredSize(new Dimension(200,600));
-		Color sc= Color.RED;
-		Color fc= Color.BLACK;
-		GradientPaint gradient=new GradientPaint(0,0,sc,100,100,fc);
-		setBackground(gradient.getColor1());
-		//Layout manager
+		setBackground(Color.RED);
 		this.setLayout(new FlowLayout());
-		//Insersion d'une icone à size modifié.
+		
+		//Inserting an icon of TELECOM PARIS .
 		ImageIcon icon = new ImageIcon("telcomParisLogo.jpg");
 		Image image = icon.getImage();
 		int width =icon.getIconWidth()/2;
@@ -36,12 +41,12 @@ public class ConfigPanel extends JPanel{
 		hField=new HeightField(mainApp);
 		wField=new WidthField(mainApp);
 		okButton= new OkButton(mainApp);
-		
+		//Setting label for width field
 		JLabel wLabel = new JLabel("Enter maze width: ");
 		wLabel.setForeground(Color.WHITE);
 		wLabel.setFont(new Font("Arial", Font.BOLD, 14));
 		wLabel.setLabelFor(wField);
-		
+		//Setting label for height field
 		JLabel hLabel = new JLabel("Enter maze height: ");
 		hLabel.setForeground(Color.WHITE);
 		hLabel.setFont(new Font("Arial", Font.BOLD, 14));
@@ -52,30 +57,33 @@ public class ConfigPanel extends JPanel{
 		this.add(wField);
 		this.add(hLabel);
 		this.add(hField);
-		
 		this.add(okButton);
 		cBoxPanel= new ChoiceBoxPanel(mainApp);
 		this.add(new JLabel("     "));
 		this.add(cBoxPanel);
-		
-		
-		
-
 	}
+	//Getters
 	public ChoiceBoxPanel getChoiceBoxPanel() {
 		return cBoxPanel;
 	}
+	public HeightField gethField() {
+		return hField;
+	}
+	public WidthField getwField() {
+		return wField;
+	}
+	public MainApp getMainApp() {
+		return mainApp;
+	}
+	
+	/**
+	 * Notifies all the components in this object that they should update their state and be repainted.
+	*/
 	public void notifyForUpdate() {
 		   okButton.notifyForUpdate() ;
 		   cBoxPanel.notifyForUpdate() ;
 		   hField.notifyForUpdate() ;
 		   wField.notifyForUpdate() ;
 	}
-	public HeightField gethField() {
-		return hField;
-	}
-
-	public WidthField getwField() {
-		return wField;
-	}
+	
 }

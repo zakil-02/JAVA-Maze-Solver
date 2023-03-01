a08ada4aad2bd6f1051b4ece2025e6b27329a919
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-03-01 12:40:57 +0100

    mise en commentaires de la classe MainPanel

--- tp04/src/graphicInterface/MainPanel.java
+++ tp04/src/graphicInterface/MainPanel.java
@@ -4,13 +4,19 @@ import javax.swing.*;
 
 import java.awt.*;
 
+/**
+ * A custom JPanel used as the main panel for our application and it will contain 2 different 
+ * panels : configurationPanel and displayPanel.
+ */
 public class MainPanel extends JPanel {
 	private final MainApp mainApp;
-	private ConfigPanel configPanel;
-	
-	private DisplayPanel displayPanel;
+	private final ConfigPanel configPanel;
+	private final DisplayPanel displayPanel;
     private static final long serialVersionUID = 1L;
-
+    
+    /**
+     * Construct a new instance of MainPanel as a new JPanel containing 2 other panels.
+     */
 	public MainPanel(MainApp mainApp) {
         super();
         this.mainApp=mainApp;
@@ -19,15 +25,19 @@ public class MainPanel extends JPanel {
         add(configPanel=new ConfigPanel(mainApp), BorderLayout.WEST);
         add(displayPanel=new DisplayPanel(mainApp),BorderLayout.CENTER);
     }
-	
+	//Getters.
 	public ConfigPanel getConfigPanel() {return configPanel;}
+	public DisplayPanel getDisplayPanel() {return displayPanel;}
+	public MainApp getMainApp() {return mainApp;}
 	
+	/**
+	 * Notifies the configuration panel and display panel that an update is needed.
+	 * This method should be called whenever changes are made to the data that
+	 * these panels display.
+	 */
 	public void notifyForUpdate() {
 		   configPanel.notifyForUpdate() ;
 		   displayPanel.notifyForUpdate();
-		   repaint();
-		}
-	public DisplayPanel getDisplayPanel() {
-		return displayPanel;
 	}
+	
 }

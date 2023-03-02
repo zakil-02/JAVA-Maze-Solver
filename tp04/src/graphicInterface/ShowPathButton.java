7de325e42e8ff46d2f9adbec83ab1c1c25b34483
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-03-02 12:11:56 +0100

    La Java doc pour la classe ShowPathButton

--- tp04/src/graphicInterface/ShowPathButton.java
+++ tp04/src/graphicInterface/ShowPathButton.java
@@ -8,12 +8,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+/**
+ * A button that, when pressed, displays the solution of the maze on the display panel.
+ */
 public class ShowPathButton extends JButton implements ActionListener {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private final MainApp mainApp;
+	/**
+     * Constructs a new ShowPathButton with the label "ShortestPath".
+     * It adds ActionListener to it.
+     * @param mainApp the MainApp object that this button is associated with.
+     */
 	public ShowPathButton(MainApp mainApp) {
 		super("ShortestPath");
 		this.mainApp=mainApp;
@@ -22,9 +27,16 @@ public class ShowPathButton extends JButton implements ActionListener {
 		this.setForeground(Color.WHITE);
 		addActionListener(this);
 	}
+	/**
+     * Handles button clicks by displaying the solution of the maze if there is a solution.
+     * @param e The ActionEvent representing the button click.
+     * @throws Exception if the number of departures or arrivals is not 1, 
+     * and handles all exceptions by showing the messages through a dialogWindow. 
+     */
 	@Override
 	public void actionPerformed(ActionEvent e){
 		try {
+			//Extracting info from the model.
 			int compteurArrivals=mainApp.getMazeAppModel().getnArrivals();
 			int compteurDeparts=mainApp.getMazeAppModel().getnDepartures();
 			if (compteurDeparts<1) {
@@ -39,10 +51,12 @@ public class ShowPathButton extends JButton implements ActionListener {
 			if (compteurArrivals>1) {
 				throw new IOException("Multiple destinations have been received");
 			}
+			//We try to solve it then.
 			mainApp.getMazeAppModel().solve();
 			
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(mainApp, "Error!\n" + ex.getMessage(), " ErrorMessage ", JOptionPane.WARNING_MESSAGE);
+			//If an error occurs, we ask the model to reset the maze.
 			mainApp.getMazeAppModel().reset(mainApp.getMazeAppModel().getWidth(), mainApp.getMazeAppModel().getHeight());
 		}
 	}

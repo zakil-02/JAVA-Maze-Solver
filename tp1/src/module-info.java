c6b3add3022baf816f2ac1553ad923792828cacc
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-03-03 18:55:49 +0100

    deleting tp1 and tp2 from repository

--- tp1/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author AKIL ZAKARIA
- *
- */
-module tp1 {
-}
--- tp1/src/tp1/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package tp1;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
-	}
-
-}

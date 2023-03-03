cbfeeb2d410450e529d394eba162754f58cd366b
Zakaria AKIL <zakaria.akil@telecom-paris.fr>
2023-03-03 18:27:25 +0100

    Documentation de Main et MainTest

--- tp04/src/mainPackage/Main.java
+++ tp04/src/mainPackage/Main.java
@@ -4,39 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import Labyrinthe.*;
 import graph.*;
-
+/**
+ * A class to contain the main method to be executed to test the Dijkstra algorithm.
+ */
 public class Main {
 	public static void main(String[] args) throws MazeReadingException, Exception{
-		//Creation d'un tableau pour représenter l'attribut boxes de Maze.
 		List<List<MazeBox>> boxes = new ArrayList<>();
 		for (int i=0; i<10;i++) {
 			boxes.add(new ArrayList<>());
 		}
-		//Instance de Maze
+		//Instance of maze
 		Maze maze = new Maze(10,10, boxes);
-		//Initialisons maze avec notre fichier text
+		//Initializing from file
 		maze.initFromTextFile("data/labyrinthe.maze");
 		
 		List<Vertex> cheminSolution=maze.getSolution().subList(1, maze.getSolution().size()-1);
-		System.out.println("Le plus court chemin à suivre :");
+		System.out.println("The path to follow:");
 		System.out.println(cheminSolution);
 		
-		System.out.println("Le resultat:");
-		//Affichage de la solution dans la console :
+		System.out.println("The result:");
 		
+		//Printing the solution in the console
 		for (int i=0;i<maze.getLength();i++) {
 			for (int j=0;j<maze.getWidth();j++) {
 				MazeBox box = maze.getBoxes().get(i).get(j);
-				//Je veux laisser visibles A et D, d'ou les 2 dernières conditions
+				//We want to keep A and D visible.
 				if (cheminSolution.contains(box)) {
-					System.out.print("\033[34m* \033[0m");//Tracage du chemin en bleu.
+					System.out.print("\033[34m* \033[0m");//Drawing the path in blue.
 				}else {
 					System.out.print(box.getLabel()+" ");
 				}
 			}
 			System.out.println();
 			if (i%2==0) {System.out.print(" ");}
-			//Tout cela pour construire la forme du labyrinthe hexagonal.	
+			//All this is to show the hexagonal aspect.	
 		}
 			
 	}
